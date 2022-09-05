da4ae8b6350287c84e903cd82e5b3467518dfa13
Saurabh12112002 <saurabh.12109637@lpu.in>
2022-09-05 16:37:55 +0530

    Learning Linked List

---
+++ Linked List/3InsertATBeginofLL.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+ struct Node
+ {
+ 	int data;
+ 	Node *next;
+ 	Node(int x)
+ 	{
+ 		data = x;
+ 		next= NULL;
+ 	}
+};
+
+Node *InsertBegin(Node *head, int x)
+{
+	Node *temp = new Node(x);
+	temp ->next = head;
+	return temp;
+}
+
+void Print(Node * head)
+{
+	Node *Current = head;
+	while(Current != NULL)
+	{
+		cout<<Current->data<<" ";
+		Current = Current->next;
+	}
+}
+ 
+
+int main()
+{
+  // Node *head = NULL;
+  Node *head = new Node(10);
+  head->next = new Node(11);
+  head->next->next = new Node(12);
+
+  head = InsertBegin(head,9);
+  head = InsertBegin(head,8);
+
+  Print(head);
+	return 0;
+}
---
+++ Linked List/4InsertATENDofLL.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+	int data;
+	Node *next;
+	Node(int x)
+	{
+		data = x;
+		next = NULL;
+	}
+};
+
+Node *InsertEnd(Node *head, int x)
+{
+	Node *temp = new Node(x);
+	if(head == NULL)
+		return temp;
+	Node *Current = head;
+	while(Current ->next != NULL)
+	{
+      Current = Current ->next;
+	}
+	Current->next = temp;
+	return head;
+}
+
+void Print(Node *head)
+{
+	Node *Current = head;
+	while(Current != NULL)
+	{
+		cout<<Current->data<<"==>";
+        Current = Current->next;
+	}
+	cout<<"NULL"<<endl;
+}
+int main()
+{
+	// Node *head = NULL;
+	Node *head = new Node(2);
+	head->next = new Node(3);
+	head->next->next = new Node(4);
+	head = InsertEnd(head, 6);
+	head = InsertEnd(head, 8);
+	Print(head);
+
+	return 0;
+}
+
+
