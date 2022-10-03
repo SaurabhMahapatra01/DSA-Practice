d688ab793b3d45908399f4d8e7cba60abff895d0
Saurabh12112002 <saurabh.12109637@lpu.in>
2022-10-04 02:43:09 +0530

    Reverse Linked List in  S Linked List by using Recursion

---
+++ Linked List/Single Linked List/14ReverseLinkedListREC.cpp
@@ -0,0 +1,71 @@
+//Time Complexity = O(n)
+//Spacce Complexity = O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+	int data;
+	Node*next;
+	Node(int x)
+	{
+		data = x;
+		next = NULL;
+	}
+};
+Node *Take_Input()
+{
+	int data;
+	cin>>data;
+
+	Node *head = NULL;
+	Node *tail = NULL;
+
+	while(data != -1)
+	{
+		Node *newNode = new Node(data);
+		if(head == NULL){
+			head = newNode;
+			tail = newNode;
+		}
+		else{
+			tail->next = newNode;
+			tail = tail->next;
+		}
+		cin>>data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *curr = head;
+	while(curr != NULL)
+	{
+		cout<<curr->data<<"=>";
+		curr = curr->next;
+	}
+	cout<<"NULL"<<endl;
+}
+
+Node *ReverseLLREC(Node *head)
+{
+  if(head == NULL || head->next == NULL){
+  	return head;
+  }
+  Node *SmallHead = ReverseLLREC(head->next);
+  head->next->next = head;
+  head->next = NULL;
+  
+  return SmallHead;
+}
+int main()
+{
+	
+  Node *head = Take_Input();
+  print(head);
+
+  head = ReverseLLREC(head);
+  print(head);
+	return 0;
+}
