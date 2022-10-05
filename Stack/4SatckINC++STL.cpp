e49bece8bf97668c3357dcb8c3417fe6a36f5645
Saurabh12112002 <saurabh.12109637@lpu.in>
2022-10-06 00:57:17 +0530

    Stack in C++ STL

---
+++ Stack/4SatckINC++STL.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+  stack<int> s;
+  s.push(10);
+  s.push(15);	
+  s.push(20);
+  // cout<<s.size()<<endl;
+  // cout<<s.top()<<endl;
+  // s.pop();
+  // cout<<s.top()<<endl;
+
+  while(s.empty() == false){
+  	cout<<s.top()<<endl;
+  	s.pop();
+  	cout<<s.size()<<endl;
+
+  }
+	return 0;
+}
