a148efa695ebf29963cf03bb80cbcb61cda7e751
Saurabh12112002 <saurabh.12109637@lpu.in>
2022-10-13 22:11:52 +0530

    Count Steps

---
+++ Recursion/Q15-CountSteps.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int helper(int num, int steps);
+int NumberOfSteps(int num)
+{
+    return helper(num, 0);
+}
+int helper(int num, int steps)
+{
+    if(num==0) {
+        return steps;
+    }
+    if(num%2==0) {
+        return helper(num/2, steps+1);
+    }
+    return helper(num-1,steps+1);
+
+}
+int main()
+{
+    int N;
+    cin>>N;
+    cout<<NumberOfSteps(N);
+}
